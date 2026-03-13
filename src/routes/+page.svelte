889ea20bb57ccc4a1c167132a9710986b579ca94
HeedlessSoap325 <mathisjulian123@gmail.com>
2026-03-13 21:52:31 +0100

    fix: make CSS for page work with svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,50 @@
-<div id="content">
-    <canvas id="qrcode"></canvas>
+<script lang="ts">
+	import { onMount } from "svelte";
 
-    <input id="input" type="text" placeholder="Qr Code Text..." value="Some Text"/>
-    <label for="errorlvl">Error Correction:</label>
-    <select id="errorlvl">
-        <option value="L">L</option>
-        <option value="M">M</option>
-        <option value="Q">Q</option>
-        <option value="H">H</option>
-    </select>
+	let canvas!: HTMLCanvasElement;
+	let ctx: CanvasRenderingContext2D | null = null;
+	let input: string = $state("Some Text");
+	let errorCorection: string = $state("L");
+
+	onMount(() => {
+		ctx = canvas.getContext("2d");
+		//TODO: draw
+	});
+
+	$effect(() => {
+		if (ctx) {
+			console.log(`rerender (input='${input}', errorCorrection='${errorCorection}')`)
+			//TODO: draw
+		}
+	});
+</script>
+
+<div id="content">
+    <canvas bind:this={canvas} id="qrcode"></canvas>
+
+    <input id="input" type="text" placeholder="Qr Code Text..." bind:value={input}/>
+
+	<div id="errorCorrectionRow">
+		<label for="errorlvl">Error Correction:</label>
+		<select id="errorlvl" bind:value={errorCorection}>
+			<option value="L">L</option>
+			<option value="M">M</option>
+			<option value="Q">Q</option>
+			<option value="H">H</option>
+		</select>
+	</div>
 </div>
 
 <style>
 	#content {
 		margin: 0;
-		display: grid;
+		display: flex;
+		flex-direction: column;
 		place-items: center;
 		height: 100vh;
 	}
 
 	#qrcode, #input {
-		display: block;
 		margin-bottom: 20px;
 	}
 </style>
