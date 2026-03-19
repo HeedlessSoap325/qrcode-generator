5ced56a2ad96bdcc0aa1aa747abf7d2c9ef95c5e
HeedlessSoap325 <mathisjulian123@gmail.com>
2026-03-19 18:50:48 +0100

    feat: choose random favicon Because its fun

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,12 @@
 <script>
-	import favicon from '$lib/assets/favicon.ico';
+	const numQrCodes = 6;
+	const num = Math.floor(Math.random() * numQrCodes);
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="icon" href={`qrCode${num}.png`} />
 </svelte:head>
 
 {@render children()}
